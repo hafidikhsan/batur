8590947af945c538d40bf507d14bbe923b3fbf9a
hafidikhsan <harifin315@gmail.com>
2024-01-19 20:06:37 +0700

    CREATE: First init

---
+++ lib/screen/on_boarding_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text("Batur"),
+          ),
+        ),
+      ),
+    );
+  }
+}
